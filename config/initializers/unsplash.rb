f3068f3bf3cbceae4c5a91538bd15bbae137ccff
Olga <pelzolga123@gmail.com>
2020-04-21 23:48:06 +0300

    Finish Unsplash API configurations

--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -1,5 +1,6 @@
 Unsplash.configure do |config|
-  config.application_access_key = ENV['fKNZ4OIg95ruXNHXEuKS91XpQONT4br-WDv7LzroRrg']
-  config.application_secret = ENV['e5u463gQDMkf2yi453TKKriyuCgu65xYkRTJnDFPk5A']
+  config.application_access_key = "fKNZ4OIg95ruXNHXEuKS91XpQONT4br-WDv7LzroRrg"
+  config.application_secret = "e5u463gQDMkf2yi453TKKriyuCgu65xYkRTJnDFPk5A"
+  config.application_redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
   config.utm_source = 'CookBook'
 end
