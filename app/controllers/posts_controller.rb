5111e2b8aca91972d46ae0648c1927bac74a20dc
Olga <pelzolga123@gmail.com>
2020-04-22 20:32:42 +0300

    Fix posts creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,36 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: %i[create destroy]
+  before_action :current_user, only: %i[create destroy]
+  before_action :set_post, only: %i[show edit update destroy]
 
   def index
-  end  
+    @posts = current_user.posts
+  end
+
+  def show; end
+  
+  def new
+    @post = Post.new
+    @posts = current_user.posts
+  end
+  
+  def create
+    @post = current_user.posts.build(post_params)
+    @post.user = current_user
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to authenticated_root_path, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { redirect_to new_post_path, alert: 'post not created' }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :image)
+  end
 end
--- db/migrate/20200422165246_create_posts.rb
+++ db/migrate/20200422165246_create_posts.rb
@@ -1,6 +1,6 @@
-class Posts < ActiveRecord::Migration[6.0]
+class CreatePosts < ActiveRecord::Migration[6.0]
   def change
-     create_table :posts do |t|
+    create_table :posts do |t|
       t.text :content
       t.string :image
       t.references :user, foreign_key: true
